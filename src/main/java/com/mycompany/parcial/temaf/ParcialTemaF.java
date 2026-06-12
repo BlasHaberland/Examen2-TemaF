35bdb4c611ab75dba27fef34abf9733b2d3d17b8
Blas <haberlandblas@gmail>
2026-06-12 16:26:12 -0300

    primer commit

---
+++ src/main/java/com/mycompany/parcial/temaf/ParcialTemaF.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parcial.temaf;
+
+/**
+ *
+ * @author Blas
+ */
+public class ParcialTemaF {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
