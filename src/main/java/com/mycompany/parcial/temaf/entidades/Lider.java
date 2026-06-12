b76022944f8f1ba1300bbd59e211ef1f1c995bdf
Blas <haberlandblas@gmail>
2026-06-12 18:26:50 -0300

    creado: Lider

---
+++ src/main/java/com/mycompany/parcial/temaf/entidades/Lider.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.parcial.temaf.entidades;
+
+/**
+ *
+ * @author Blas
+ */
+public class Lider extends Trabajador {
+
+    public Lider(String id, String nombre) {
+        super(id, nombre, "Líder de Cuadrilla");
+    }
+
+    public void cargarFechaFinal(String fecha) {
+        System.out.println("El líder " + this.nombre + " cargó la fecha final de obra: " + fecha);
+    }
+
+    public void cargarReparacion(String detalles, String hora) {
+        System.out.println("El líder " + this.nombre + " reportó la reparación: " + detalles + " a las " + hora);
+    }
+}
