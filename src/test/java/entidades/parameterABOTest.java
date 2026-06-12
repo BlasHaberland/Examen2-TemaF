a5ba6e79cbba6c61a17f392ac020d3204cf9eb24
Blas <haberlandblas@gmail>
2026-06-12 18:01:13 -0300

    agregado: parameterABOTest

--- src/test/java/entidades/parameterABOTest.java
+++ src/test/java/entidades/parameterABOTest.java
@@ -4,10 +4,45 @@
  */
 package entidades;
 
+import com.mycompany.parcial.temaf.entidades.Abonado;
+import java.util.Arrays;
+import static junit.framework.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
 /**
  *
  * @author Blas
  */
+@RunWith(value = Parameterized.class)
 public class parameterABOTest {
 
+    private String emailAProbar;
+    private boolean resultadoEsperado;
+
+    public parameterABOTest(String emailAProbar, boolean resultadoEsperado) {
+        this.emailAProbar = emailAProbar;
+        this.resultadoEsperado = resultadoEsperado;
+    }
+
+    // Datos dee preueba
+    @Parameters
+    public static Iterable<Object[]> tomarDatos() {
+        return Arrays.asList(new Object[][]{
+            {"juan@mail.com", true},
+            {"correo_sin_arroba.com", false},
+            {"correo@sinpunto", false},
+            {"no se puede mail con espasios@gmail.com", false},
+            {"abonado.ok@cctve.com.ar", true}
+        });
+    }
+
+    @Test
+    public void testValidarEmail() {
+        boolean resultadoReal = Abonado.ValidarMail(this.emailAProbar);
+
+        assertEquals("Fallo evaluando el email: " + this.emailAProbar, this.resultadoEsperado, resultadoReal);
+    }
 }
