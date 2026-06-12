002bfb4504b56284c1246bc972f08b14edf2f231
Blas <haberlandblas@gmail>
2026-06-12 18:24:24 -0300

    creado: Trabajador

---
+++ src/main/java/com/mycompany/parcial/temaf/entidades/Trabajador.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.parcial.temaf.entidades;
+
+/**
+ *
+ * @author Blas
+ */
+public class Trabajador {
+
+    protected String id;
+    protected String nombre;
+    protected String puesto;
+    protected String estado;
+
+    public Trabajador(String id, String nombre, String puesto) {
+        this.id = id;
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.estado = "libre";
+    }
+
+    public void setEstado(String nuevoEstado) {
+        this.estado = nuevoEstado;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+}
